e8d54614676c2070374b1ef52c0a376ace4e8ced
PantsuDev <ewhal@pantsu.cat>
2018-01-16 12:07:10 +1000

    Add in thumbnailing functionality

---
+++ utils/resize/resize.go
@@ -0,0 +1,69 @@
+package resize
+
+import (
+	"github.com/astaxie/beego/logs"
+	"github.com/nfnt/resize"
+	"image/jpeg"
+	"image/png"
+	"os"
+)
+
+func ResizePng(open string, filepath string) {
+	log := logs.GetLogger()
+	file, err := os.Open(open)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	// decode png into image.Image
+	img, err := png.Decode(file)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	// resize to width 60 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(200, 0, img, resize.Lanczos3)
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer out.Close()
+
+	// write new image to file
+	png.Encode(out, m)
+}
+
+func ResizeJpg(open string, filepath string) {
+	log := logs.GetLogger()
+	file, err := os.Open(open)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	// decode jpeg into image.Image
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	file.Close()
+
+	// resize to width 1000 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(200, 0, img, resize.Lanczos3)
+
+	out, err := os.Create("test_resized.jpg")
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer out.Close()
+
+	// write new image to file
+	jpeg.Encode(out, m, nil)
+}
