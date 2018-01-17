5e8ccc823515f2bf1a3cc0362c41638bc6b574c1
Elliot Brock <beta.redex@gmail.com>
2018-01-17 13:56:25 -0500

    add rss controller and a function to get recent chapters from followed series

--- models/series_user_following.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type SeriesUserFollowing struct {
-	Id       int     `orm:"auto"`
-	SeriesId *Series `orm:"column(series_id);rel(fk)"`
-	UserId   *Users  `orm:"column(user_id);rel(fk)"`
-}
