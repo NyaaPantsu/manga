396a05eea4f9d44fabf92399542c7813b5124aee
Elliot Brock <beta.redex@gmail.com>
2018-01-17 20:28:47 -0500

    fix a ton of bugs

--- controllers/rss.go
+++ controllers/rss.go
@@ -11,7 +11,7 @@ import (
 
 // RssController operations for Rss
 type RssController struct {
-	beego.Controller
+	BaseController
 }
 
 // URLMapping ...
@@ -25,12 +25,12 @@ func (c *RssController) URLMapping() {
 // GetNew ...
 // @Title GetNew
 // @Description get recently updated series
-// @Param	id		path 	string	true		"The key for staticblock"
+// @Param id		path	string	true		"The key for staticblock"
 // @Success 200 {object} models.Rss
 // @Failure 403 :id is empty
 // @router /:id [get]
 func (c *RssController) GetNew() {
-	flash = beego.NewFlash()
+	flash := beego.NewFlash()
 
 	recents, err := models.GetRecentSeriesByChapter(0, 50)
 	if err != nil {
@@ -47,16 +47,16 @@ func (c *RssController) GetNew() {
 		Description: "Recently updated manga",
 		Created: now,
 	}
-	feed.Items := make([]*feeds.Item, 50)
+	feed.Items = make([]*feeds.Item, 50)
 	for index, element := range recents {
-		feed.Items[index] := &feeds.Item{
-			Title: fmt.Sprintf("%s - Chapter %d", element.Series.Title, element.ChapterNumberAbsolute),
+		feed.Items[index] = &feeds.Item{
+			Title: fmt.Sprintf("%s - Chapter %d", element.SeriesId.Name, element.ChapterNumberAbsolute),
 			Link: &feeds.Link{Href: fmt.Sprint("https://manga.sh/reader/", element.Hash)},
 		}
 	}
 	switch c.GetString("format") {
 	case "atom":
-		atom, err = feed.ToAtom()
+		atom, err := feed.ToAtom()
 		if err != nil {
 			flash.Error("Atom generation failed")
 			flash.Store(&c.Controller)
@@ -64,9 +64,9 @@ func (c *RssController) GetNew() {
 			return
 		}
 		c.Ctx.Output.Header("Content-Type", "application/atom+xml")
-		c.Ctx.Output.Body(atom)
+		c.Ctx.Output.Body([]byte(atom))
 	case "rss":
-		rss, err = feed.ToRSS()
+		rss, err := feed.ToRss()
 		if err != nil {
 			flash.Error("RSS generation failed")
 			flash.Store(&c.Controller)
@@ -74,9 +74,9 @@ func (c *RssController) GetNew() {
 			return
 		}
 		c.Ctx.Output.Header("Content-Type", "application/rss+xml")
-		c.Ctx.Output.Body(rss)
+		c.Ctx.Output.Body([]byte(rss))
 	case "json":
-		json, err = feed.ToJSON()
+		json, err := feed.ToJSON()
 		if err != nil {
 			flash.Error("JSON generation failed")
 			flash.Store(&c.Controller)
@@ -84,31 +84,32 @@ func (c *RssController) GetNew() {
 			return
 		}
 		c.Ctx.Output.Header("Content-Type", "application/json")
-		c.Ctx.Output.Body(json)
+		c.Ctx.Output.Body([]byte(json))
 	}
+	return
 }
 
 // GetFollowed ...
 // @Title GetFollowed
 // @Description get recently updated series from followed series
-// @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
-// @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
-// @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
-// @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
-// @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
-// @Param	offset	query	string	false	"Start position of result set. Must be an integer"
+// @Param query query string	false "Filter. e.g. col1:v1,col2:v2 ..."
+// @Param fields	query string	false "Fields returned. e.g. col1,col2 ..."
+// @Param sortby	query string	false "Sorted-by fields. e.g. col1,col2 ..."
+// @Param order query string	false "Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
+// @Param limit query string	false "Limit the size of result set. Must be an integer"
+// @Param offset	query string	false "Start position of result set. Must be an integer"
 // @Success 200 {object} models.Rss
 // @Failure 403
 // @router / [get]
 func (c *RssController) GetFollowed() {
-	flash = beego.NewFlash()
+	flash := beego.NewFlash()
 
-  if(!c.IsLogin){
-    flash.Error("You must be logged in to view followed series")
-    flash.Store(&c.Controller)
-    c.Redirect("/auth/login", 302)
-    return
-  }
+	if !c.IsLogin {
+		flash.Error("You must be logged in to view followed series")
+		flash.Store(&c.Controller)
+		c.Redirect("/auth/login", 302)
+		return
+	}
 
 	recents, err := models.GetRecentFollowedSeriesByChapter(c.GetSession("userinfo").(int), 0, 50)
 	if err != nil {
@@ -125,16 +126,16 @@ func (c *RssController) GetFollowed() {
 		Description: "Recently updated followed manga",
 		Created: now,
 	}
-	feed.Items := make([]*feeds.Item, 50)
+	feed.Items = make([]*feeds.Item, 50)
 	for index, element := range recents {
-		feed.Items[index] := &feeds.Item{
-			Title: fmt.Sprintf("%s - Chapter %d", element.Series.Title, element.ChapterNumberAbsolute),
+		feed.Items[index] = &feeds.Item{
+			Title: fmt.Sprintf("%s - Chapter %d", element.SeriesId.Name, element.ChapterNumberAbsolute),
 			Link: &feeds.Link{Href: fmt.Sprint("https://manga.sh/reader/", element.Hash)},
 		}
 	}
 	switch c.GetString("format") {
 	case "atom":
-		atom, err = feed.ToAtom()
+		atom, err := feed.ToAtom()
 		if err != nil {
 			flash.Error("Atom generation failed")
 			flash.Store(&c.Controller)
@@ -142,9 +143,9 @@ func (c *RssController) GetFollowed() {
 			return
 		}
 		c.Ctx.Output.Header("Content-Type", "application/atom+xml")
-		c.Ctx.Output.Body(atom)
+		c.Ctx.Output.Body([]byte(atom))
 	case "rss":
-		rss, err = feed.ToRSS()
+		rss, err := feed.ToRss()
 		if err != nil {
 			flash.Error("RSS generation failed")
 			flash.Store(&c.Controller)
@@ -152,9 +153,9 @@ func (c *RssController) GetFollowed() {
 			return
 		}
 		c.Ctx.Output.Header("Content-Type", "application/rss+xml")
-		c.Ctx.Output.Body(rss)
+		c.Ctx.Output.Body([]byte(rss))
 	case "json":
-		json, err = feed.ToJSON()
+		json, err := feed.ToJSON()
 		if err != nil {
 			flash.Error("JSON generation failed")
 			flash.Store(&c.Controller)
@@ -162,6 +163,7 @@ func (c *RssController) GetFollowed() {
 			return
 		}
 		c.Ctx.Output.Header("Content-Type", "application/json")
-		c.Ctx.Output.Body(json)
-  }
+		c.Ctx.Output.Body([]byte(json))
+	}
+	return
 }
--- models/users_following_series.go
+++ models/users_following_series.go
@@ -1,14 +1,19 @@
 package models
 
+import (
+	"github.com/astaxie/beego/orm"
+)
+
 type UsersFollowingSeries struct {
 	UserId   *Users  `orm:"column(user_id);rel(fk)"`
 	SeriesId *Series `orm:"column(series_id);rel(fk)"`
 }
 
 //GetRecentFollowedSeriesByChapter Gets followed Series in order of chapter updates
-func GetRecentFollowedSeriesByChapter(userId UserId, offset int, limit int) (b []SeriesChapter, err error) {
-  o := orm.NewOrm()
-  var series orm.ParamsList
-  _, err = o.QueryTable("users_following_series").Filter("user_id", userId).ValuesFlat(&series, "series_id")
-  _, err = o.QueryTable("series_chapters").Filter("series_chapters__series_id__in", series...).OrderBy("time_uploaded").Limit(limit, offset).RelatedSel().All(&b)
+func GetRecentFollowedSeriesByChapter(user_id int, offset int, limit int) (b []SeriesChapters, err error) {
+	o := orm.NewOrm()
+	var series orm.ParamsList
+	_, err = o.QueryTable("users_following_series").Filter("user_id", user_id).ValuesFlat(&series, "series_id")
+	_, err = o.QueryTable("series_chapters").Filter("series_chapters__series_id__in", series...).OrderBy("time_uploaded").Limit(limit, offset).RelatedSel().All(&b)
+  return
 }
